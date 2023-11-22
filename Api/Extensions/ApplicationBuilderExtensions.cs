a487802e7805cf0c0438d2a036a2fbbf86fb7855
DanieleSeverin89 <daniele.severin@hbtsa.it>
2023-11-22 22:19:04 +0100

    Exception handling;

--- Api/Extensions/ApplicationBuilderExtensions.cs
+++ Api/Extensions/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Infrastructure;
+﻿using Api.Middleware;
+using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api.Extensions;
@@ -14,8 +15,8 @@ public static class ApplicationBuilderExtensions
         dbContext.Database.Migrate();
     }
 
-    //public static void UseCustomExceptionHandler(this IApplicationBuilder app)
-    //{
-    //    app.UseMiddleware<ExceptionHandlingMiddleware>();
-    //}
+    public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
 }
